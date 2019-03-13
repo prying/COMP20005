51fbc813ef8937c6c4857d6b1ba33d78f1869c98
Flynn Harrison <flynn@belmontharrison.net>
2019-03-14 00:17:21 +1100

    updated for 4.7

--- mywc/main.c
+++ mywc/main.c
@@ -7,18 +7,39 @@
 
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char** argv)
 {
     int c = 0,
         line_count = 1,
-        char_count = 0;
+        char_count = 0,
+        word_count = 0;
+        
+    bool inword = false;
 
     printf("Enter text:\n");
 
     // Count input 
     while ((c = getchar()) != EOF)
     {
+        // Word count
+        if ((c >= 'A' && c >= 'Z' ) || (c >= 'a' && c >= 'z' ))
+        {
+            if (inword == false)
+                inword = true;
+
+        }
+        else
+        {
+            if (inword == true)
+            {
+                inword = false;
+                word_count++;
+            }
+        }
+        
+
         if (c == '\n')
         {
             // this is somewhat flawed as one can hit enter multiple times
@@ -30,6 +51,10 @@ int main(int argc, char** argv)
         }
     }
 
-    printf("\nCharacters: %d\nLines: %d", char_count, line_count);
+    // If exited after a word with no other char
+    if (inword == true)
+        word_count++;
+
+    printf("\nCharacters: %d\nWords: %d\nLines: %d", char_count, word_count, line_count);
     return 0;
 }
