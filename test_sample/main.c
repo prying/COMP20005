9df7afb7091ca94041d1b860b7ae74661b815b73
Flynn Harrison <flynn@belmontharrison.net>
2019-03-28 22:46:08 +1100

    for Joe

---
+++ test_sample/main.c
@@ -0,0 +1,26 @@
+/*****************************
+ * Flynn Harrison 13/03/2019
+ * Creates a bar graph from
+ * an arbitrary lenght input
+ * 
+ ****************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    int input = 0;
+    int pre_input = 0;
+
+    while(scanf("%d", &input) == 1)
+    {
+        if(input < pre_input){
+            printf("*** value %d is smaller than %d", input, pre_input);
+        }
+        pre_input = input;
+    }
+
+    return 0;
+}
