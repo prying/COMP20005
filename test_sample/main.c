88bfa4e7564bf11503c8d864ae39b65afd20b841
Flynn Harrison <flynn@belmontharrison.net>
2019-03-28 22:50:49 +1100

    updated code comment for Joe

--- test_sample/main.c
+++ test_sample/main.c
@@ -1,8 +1,9 @@
 /*****************************
  * Flynn Harrison 13/03/2019
- * Creates a bar graph from
- * an arbitrary lenght input
- * 
+ * Example code for a example
+ * question.
+ * This code compares the previous
+ * input to the current one 
  ****************************/
 
 
@@ -17,9 +18,11 @@ int main(int argc, char** argv)
     // While there is an input get it
     while(scanf("%d", &input) == 1)
     {
-        if(input < pre_input){
+        if(input < pre_input)
+        {
             printf("*** value %d is smaller than %d", input, pre_input);
         }
+        // Store the input 
         pre_input = input;
     }
 
